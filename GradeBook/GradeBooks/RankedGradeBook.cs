9334fe9fabce6bd5f0b9cb7da4b82f959197f1fe
Kevin Bushman <kevin.bushman@citrix.com>
2018-05-01 14:06:08 -0400

    ranked needs to be public

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -3,7 +3,7 @@ using GradeBook.GradeBooks;
 
 namespace GradeBook.GradeBooks
 {
-    class RankedGradeBook : BaseGradeBook
+    public class RankedGradeBook : BaseGradeBook
     {
         public RankedGradeBook(string name) : base(name)
         {
