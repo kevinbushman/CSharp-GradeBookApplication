eaf0c7dda9a8a5410de7b52794ee986d893825ee
Kevin Bushman <kevin.bushman@citrix.com>
2018-05-10 15:32:58 -0400

    Override Statistics

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -33,5 +33,16 @@ namespace GradeBook.GradeBooks
                 else
                     return 'F';
         }
+
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked Grading requires at least 5 students");
+                return;
+            }
+
+            base.CalculateStatistics();
+        }
     }
 }
