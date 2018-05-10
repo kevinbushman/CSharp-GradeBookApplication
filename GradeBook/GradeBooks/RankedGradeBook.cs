3fb5283a760657518d6ecf8ca8d835cbc907156d
Kevin Bushman <kevin.bushman@citrix.com>
2018-05-10 15:36:27 -0400

    Missing words

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -38,7 +38,7 @@ namespace GradeBook.GradeBooks
         {
             if (Students.Count < 5)
             {
-                Console.WriteLine("Ranked Grading requires at least 5 students");
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
                 return;
             }
 
